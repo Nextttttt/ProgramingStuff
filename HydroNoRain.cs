5a570ad0bb9774fb78b29f6f8e3a0f883215f61f
Nextttttt <72907943+Nextttttt@users.noreply.github.com>
2021-01-31 22:21:44 +0200

    Add files via upload

---
+++ HydroNoRain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace HydroNoRain
+{
+    class HydroNoRain
+    {
+        static void EnterHydroArray(int n, double[] Array)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Insert HydroGraph Data for {i} Day:");
+                Array[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        static int NoRainyDays(int n, double[] Array)
+        {
+            int daysWithoutRain = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (Array[i] == 0)
+                {
+
+                    daysWithoutRain ++;
+
+                }
+            }
+            return daysWithoutRain;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert days in month:");
+            int daysCount = int.Parse(Console.ReadLine());
+            double[] A = new double[daysCount];
+            double[] B = new double[daysCount];
+            double[] C = new double[daysCount];
+
+            EnterHydroArray(daysCount, A);
+            EnterHydroArray(daysCount, B);
+            EnterHydroArray(daysCount, C);
+
+            Console.WriteLine($"Days without rain for Station 1: {NoRainyDays(daysCount, A)}");
+            Console.WriteLine($"Days without rain for Station 2: {NoRainyDays(daysCount, B)}");
+            Console.WriteLine($"Days without rain for Station 3: {NoRainyDays(daysCount, C)}");
+
+            for(int i = 0; i <= daysCount; i ++)
+            {
+
+                if(A[i] != 0 || B[i] != 0  || C[i] != 0)
+                {
+
+                    Console.Write($"Rainy days: {i}");
+
+                }
+
+            }
+        }
+
+        }
+    }
